b98491915d1daec315257c63739c2ce2da3989d5
jeffreyshih11 <j.shih11@gmail.com>
2016-12-03 17:11:51 -0500

    Update DealGetter.java

--- HappyHokie/app/src/main/java/com/example/jeff/happyhokie/DealGetter.java
+++ HappyHokie/app/src/main/java/com/example/jeff/happyhokie/DealGetter.java
@@ -13,6 +13,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
  * Created by Jeff on 12/2/2016.
  */
 
+//
 public class DealGetter {
 
     String sDay;
@@ -64,7 +65,7 @@ public class DealGetter {
                         for (int x = 0; x < ddeals.getLength(); x++) {
                             Element DrinkDeal = (Element) ddeals.item(x);
                             //System.out.println("\t\tDrink:" + DrinkDeal.getTextContent());
-                            f.addDrink(DrinkDeal.getTextContent());
+                            f.addDrink(DrinkDeal.getAttribute("name"));
                         }
                     }
                 }
@@ -113,7 +114,13 @@ public class DealGetter {
                                 for (int x = 0; x < ddeals.getLength(); x++) {
                                     Element DrinkDeal = (Element) ddeals.item(x);
                                     //System.out.println("\t\tDrink:" + DrinkDeal.getTextContent());
-                                    f.addDrink(DrinkDeal.getTextContent());
+                                    f.addDrink(DrinkDeal.getAttribute("name"));
+
+                                    NodeList details = DrinkDeal.getElementsByTagName("Detail");
+                                    for(int m = 0; m < details.getLength(); m++){
+                                        f.addDrinkDetails(details.item(m).getTextContent());
+                                        //System.out.println(details.item(m).getTextContent());
+                                    }
                                 }
                             }
                         }
@@ -133,19 +140,19 @@ public class DealGetter {
     //for refresh or for reuse to get all deals at one place
     public String setDay(int newDay){
         switch (newDay){
-            case 1: return "Monday";
+            case 1: return "Sunday";
 
-            case 2: return "Tuesday";
+            case 2: return "Monday";
 
-            case 3: return "Wednesday";
+            case 3: return "Tuesday";
 
-            case 4: return "Thursday";
+            case 4: return "Wednesday";
 
-            case 5: return "Friday";
+            case 5: return "Thursday";
 
-            case 6: return "Saturday";
+            case 6: return "Friday";
 
-            case 7: return "Sunday";
+            case 7: return "Saturday";
 
         }
         return null;
