b3cebeb46df04c17e41d7f9465007787aa2e492d
jeffreyshih11 <j.shih11@gmail.com>
2016-12-06 04:22:41 -0500

    Update MainActivity.java

--- HappyHokie/app/src/main/java/com/example/jeff/happyhokie/MainActivity.java
+++ HappyHokie/app/src/main/java/com/example/jeff/happyhokie/MainActivity.java
@@ -5,14 +5,19 @@ package com.example.jeff.happyhokie;
  * application
  */
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,20 +28,35 @@ public class MainActivity extends AppCompatActivity {
     List<String> expandableListTitle;
     HashMap<String, List<String>> expandableListDetail;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        InputStream data = getResources().openRawResource(R.raw.data);
+        DealGetter dealGetter = new DealGetter(day, data);
+        ArrayList<FullDeal> dealList = dealGetter.getAllDeals();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
-        expandableListDetail = ExpandableListDataPump.getData();
-        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
-        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        //expandableListDetail = ExpandableListDataPump.getData();
+        for(int i = 0;i<dealList.size();i++) {
+            expandableListTitle = new ArrayList<String>();
+            expandableListTitle.add(dealList.get(i).getRestaurant());
+        }
+        System.out.println(dealList.get(0).getRestaurant());
+        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, getResources().openRawResource(R.raw.data));
         expandableListView.setAdapter(expandableListAdapter);
         expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
             @Override
             public void onGroupExpand(int groupPosition) {
-              //What happens when a group is expanded
+                //What happens when a group is expanded
             }
         });
 
@@ -57,7 +77,18 @@ public class MainActivity extends AppCompatActivity {
                                         int groupPosition, int childPosition, long id) {
 
                 //TODO: Intent to more details page
-                return false;
+                Intent intent = new Intent(MainActivity.this, DescriptionActivity.class);
+                FullDeal newA = (FullDeal) expandableListAdapter.getGroup(groupPosition);
+
+                String title = newA.getRestaurant();
+
+                System.out.println(title);
+                // Send Restaurant name param
+                intent.putExtra("REST_TITLE", title);
+                System.out.println("just pushed title\n");
+                startActivity(intent);
+                return true;
+                //return false;
             }
         });
     }
